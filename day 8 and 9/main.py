63de795f121aba924384b14c3f3a727b49339ea9
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-21 16:24:38 +0545

    it is about type casting pr conversipm

---
+++ day 8 and 9/main.py
@@ -0,0 +1,14 @@
+# typecasting in python 
+a="5";
+c=5;
+d=6;
+b="6";
+print(a+b);# it is conmcation 
+print(c+d); # it is adding ;
+print(int(a)+int(b)) # a and b is converted to int , as the string that hold is a number type 
+
+#implicit type casting 
+a=1.32 ;
+b=2;
+print( int (a )+ float (b)) # here a is converted into float and b is converted into float
+
