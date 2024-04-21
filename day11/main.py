d624585fa483c72aea70cc17aa0380e6c0fe8366
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-21 19:26:26 +0545

    this is my  first commit

---
+++ day11/main.py
@@ -0,0 +1,14 @@
+# learning abput string 
+name="sandeep";
+print("my name is ",name)
+fruits=''' apple ''',""" watermelon"""
+print("fruits" ,fruits);
+#indexing in python ;
+print(name[0],name[1]) #it take a single character of the name as a string 
+print(name[2])
+print(name[3]) #it take a single character of the name as a string 
+
+# for loop
+# using for loop for printinh the single character of the name 
+for character in name:
+    print(character)
