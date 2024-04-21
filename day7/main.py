a09d0b91ecfb93fe70cd320c3a25b0d0f200fbee
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-21 15:52:01 +0545

    this is day 7

---
+++ day7/main.py
@@ -0,0 +1,26 @@
+# calculation 6using python
+# operators 
+# print(5+6) ;
+# print(5/6) ;
+# print(51-6) ;
+# print(5*6) ;
+# print(5 % 6) ;  #@modulor division shows only remainder
+# print(2**3) ; # for sxponential i.e 5^6 
+
+#calculator 
+print("enter the two numbers");
+a=26;
+print("a = ",a);
+b=13 ;
+print("b = ",b);
+print("division of a and b is " ,a/b )
+print("add is " ,a+b )
+print("multiplication is ", a*b )
+print("subtraction of a and b  is " ,a-b )
+
+
+
+
+
+
+
