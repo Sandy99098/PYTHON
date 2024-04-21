6fbe43f504ed7175957185dc537aabb3e18fd17e
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-21 18:46:10 +0545

    taking input from user

---
+++ day10/main.py
@@ -0,0 +1,14 @@
+# taking input from the user 
+# print("enter the value of a")  # can also give inside the input's parenthesis 
+# a=input(); 
+# a=input("enter the value of a \t");
+# b=input("enter the value of b \t");
+# print("the value of a + b is ",a+b);
+# #   in the python inside the input parenthesis ,it is a string  so we have to do type cast if the entered value is integer or othe datatypes
+
+x=input("first number  ? \t" );
+y=input("second number  ? \t" );
+print("sum is   ", int(x)+int(y)); #we have to type casting because inpute takes  string values as input 
+print("divide is   ", int(x)/int(y)); 
+print("multiply is   ", int(x)*int(y)); 
+print("subtraction of " ,x ,"-", y,"is " ,int(x)-int(y)); 
