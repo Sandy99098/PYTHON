dab3216b1201e93da8ef187ed4a2617f6ed416a2
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-23 16:13:27 +0545

    for loop

---
+++ day17/main.py
@@ -0,0 +1,22 @@
+# looping in pyhton
+
+#for loop :
+# name="ram"
+# for i in name :
+#     print(i);
+
+# name =["hello,ram" ]
+# for i  in name :
+#     print(i) ;
+
+
+#to print mutiple time with range 
+for i in range(5):
+    print("hello World",i);
+    print(i);
+    print(i+1)
+
+#for with 3 paramater in range
+for i in range(0,6,2): #range(start,upto,steps)
+    print(i);
+    
