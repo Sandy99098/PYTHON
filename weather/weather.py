324abbf331332adc6ea4d27bc353f8efb72a0811
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-23 09:09:14 +0545

    weathe app

--- weather/weather.py
+++ weather/weather.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-api_key = 'enter your api '
+api_key = 'enter your api'
 latitude = '28.2139'  # Latitude of pokhara, Nepal
 longitude = '83.9922'  # Longitude of pokhara, Nepal
 url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric'
