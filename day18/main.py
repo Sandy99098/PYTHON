f7cfe6b8b7d7b474e3be276a0257c15890149ec0
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-23 16:33:06 +0545

    while loop

---
+++ day18/main.py
@@ -0,0 +1,34 @@
+# while loop 
+#execute the program until it is true
+# i=0
+# while(i<3):
+#     print(i)
+#     i=i+1
+i=0;
+while(i<=8):
+    print(i)
+    i=i+1
+
+# i=int(input("ente the i"));
+i=0
+while (i<=7):
+    i=input("enter the number")
+    i=int(i)
+    print(i)
+print("loop is done,value is greater than 7")
+
+#decrementing loop
+count=8
+while(count>0):
+    print(count);
+    count=count-1;
+
+#else while loop
+count=-8
+while(count>0):
+    print(count);
+    count=count-1;
+else:
+    print("tne program is terminated")
+
+
