0f2986208a1600ec6da2f748570b0b9cc061359a
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-27 18:55:04 +0545

    f string uses

--- day28_fstrings/main.py
+++ day28_fstrings/main.py
@@ -1,1 +1,18 @@
 #fstrings in python
+sentence="Hey I am {} and I am from {}";
+# name=input("Enter your name \t")
+name="Sand"
+# country=input("Enter your country name \t")
+country="america"
+# sentence="Hey I am {0} and I am from {1}";
+print(sentence.format(name,country))
+
+print(f" Hey I am {name} and I am from {country}")
+
+price=40.29302;
+txt=f"for only {price:.2f}" #to print only two digit after decimal.must use .
+# txt=f"for only {{price:.2f}} #to print only two digit after decimal.must use .
+
+print(txt)
+# f string 
+print(f"{2+3}")
