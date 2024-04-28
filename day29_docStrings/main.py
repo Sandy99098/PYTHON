b5806c8979428e2e8841c133b3e46ccbb3d2a957
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-28 17:52:29 +0545

    dosctrings in python& PEP8

---
+++ day29_docStrings/main.py
@@ -0,0 +1,14 @@
+# doc strings in python.helps to understand the intended the fuunctionality of the function
+#it is written just above the  function defination
+
+def square(a):
+    ''' takes a value in a and returns the square of a'''
+    print("square is ",a**2)
+square(2)
+print(square.__doc__)
+
+
+
+#PEP 8 (Python Enhancement Proposal)
+#helps to make program readable and understandable
+# import this
