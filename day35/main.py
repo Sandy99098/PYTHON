aca2f0508919d013a4fef1e1dd2c1b87e4835ff6
Sandy99098 <waglesandeeep8@gmail.com>
2024-04-30 18:05:31 +0545

    using else in loop

---
+++ day35/main.py
@@ -0,0 +1,28 @@
+# for looop with else (imp)
+
+for i in range(6):
+    print(i)
+    if i==4:
+        break
+else:
+    print(" loop ended ")
+
+for i in range(4):
+    print(i)
+
+else:
+    print(" loop ended ")
+    
+#eg
+for i in []:
+    print(i)
+   
+else:
+    print(" loop not executed ")
+#while loop
+i=0;
+while i<7:
+    print(i)
+    i=i+1
+else:
+    print("While Loop ended, it is executed after while loop executed up to 7 ")
