474c6e1c1eb21ec19daf147494ac2f2c498d5015
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-03 21:29:49 +0545

    os modules in python

---
+++ day46_osModule/main.py
@@ -0,0 +1,17 @@
+# os module in python
+import os
+# os.mkdir("day46_osModule/python practice")
+
+# for i in range(2):
+#     os.mkdir(f"day46_osModule/python practice/day{i+1}")
+
+# #rename 
+# for i in range(2):
+#     os.rename(f"day46_osModule/python practice/day{i+1}",f"day46_osModule/python practice/Tutorial day{i+1}")
+
+# folders=os.listdir("day46_osModule")
+# # print(folders)
+
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"python practice\{folder}"))
