37d903cc88bcc7632f71516dfce1eb96862637fa
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-03 19:26:20 +0545

    if __name__=='__main__ '

---
+++ day45/main.py
@@ -0,0 +1,3 @@
+import sandy as s
+
+s.welcome()
---
+++ day45/sandy.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Welcome from sandeep")
+print(__name__)
+sandeep=print("Good Person ")
+
+if __name__=="__main__":
+    (welcome())
