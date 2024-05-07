000acf1cca1d2f5f1d3a84b5337b0076c749b109
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-07 21:27:21 +0545

    is vs ==

---
+++ day54/main.py
@@ -0,0 +1,9 @@
+# is key word / it record exact location of the object
+
+# a=4 # compare the  value
+# b=4  
+# python stores the value of 4 in same memory location
+a=[1,2,3]
+b=[1,2,3]
+print(a is b) #compares the identity
+print(b == a) # compare the value
