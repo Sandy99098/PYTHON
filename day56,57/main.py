82c48e6711400197a52c10a07b35bb49280243e7
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-08 20:46:15 +0545

    about oop class

---
+++ day56,57/main.py
@@ -0,0 +1,28 @@
+#   reason of using oop 
+#  classes and  objects  in python ;
+#  examples
+
+# class Person:
+#       Name=" Sandeep"
+#       Occupation ="Developer"
+#       Income=9998
+
+# a=Person()
+# print(a.Name,a.Income,a.Occupation)
+
+class Person:
+      Name=" Sandeep"
+      Occupation ="Developer"
+      Income=9998
+      def info(self): # self is 
+          print(f" {self.Name} is a {self.Occupation}")
+
+a=Person()
+b=Person()
+a.Occupation="doing well and i am doing well and "
+a.info()
+b.Name="Harry "
+b.Occupation=" Developer  "
+b.Income= 90339
+b.info()
+
