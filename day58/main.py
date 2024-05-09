a6012fa9beeea3b45a76114b1b55c6dce3aa9ca9
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-09 22:13:09 +0545

    constructors in Python

---
+++ day58/main.py
@@ -0,0 +1,32 @@
+#Constructor in python
+# class person:
+    
+#     name="Sandeep"
+#     occ="Designer"
+        
+#     def info(self):
+#         print(f" I am {self.name} and my job is {self.occ}")
+
+# a=person()
+# a.info()
+
+# Example of constructor 
+class person:
+    #  # Default constructor
+    # def __init__(self,n="hari",o="Coder"):
+        # self.name=n
+        # self.occ=o
+    #    print("I am a Designer")
+    
+    #Paramaterized constructor
+    def __init__(self,n,o): 
+       self.name=n
+       self.occ=o
+   
+        
+    def info(self):
+        print(f" I am {self.name} and my I am  {self.occ}")
+
+a=person("Sandeep","Developer") # constructor ma 3 ta pass vaaxa 1ta a obj and other are paranaters
+a.info()
+p=person()
