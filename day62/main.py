cbc5e3660b2f3066f8608f408c9838476306f68a
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-10 19:05:30 +0545

    Access specifiers in python

--- day62/main.py
+++ day62/main.py
@@ -1,1 +1,42 @@
 # Your Python code goes here
+
+#ACess specifers  or modifiers in Python
+
+class hey:
+    def __init__(self):
+        self.name="naam mero "
+        
+a=hey()
+print(a.name)
+
+# Private 
+
+class hey:
+    def __init__(self):
+        self.__name="naam mero "
+        
+a=hey()
+#  it can be access indirectly .by name mangling 
+# print(a.___name) #we cannot access because name act as a private  
+
+
+# Name MANGLING  cannot be accessed directly 
+#  using (_CLASSNAME__(value tha you wanna acess) )
+# , accessing private  class
+class hey:
+    def __init__(self):
+        self.__name="naam mero "
+        
+a=hey()
+#  it can be access indirectly .by name mangling 
+print(a._hey__name) #we cannot access because name act as a private  
+
+
+
+# protected   single underscore is just a convention which denote it is protected
+class prot:
+    def __init__(self):
+        self._name="Protected "
+        
+b=prot()
+print(b._name)
