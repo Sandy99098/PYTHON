b63dc3988c5b2e4a81e732bc6897318d91439e10
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-11 15:12:02 +0545

    Ex 6 Library Management Software in Python

--- day66/main.py
+++ day66/main.py
@@ -6,14 +6,16 @@ class Employee:
     number=0
     def __init__(self ,name):
         self.name=name
+        self.raiseamt=2
         Employee.number+=1
     def info(self):
-        print(f"{self.name} works at{self.company } and total employess are {Employee.number}") 
+        print(f"{self.name}'s raise amount is {self.raiseamt} and  works at{self.company } and total employess are {Employee.number}") 
         # print(f"{self.name} works at {Employee.company} ") 
 
 emp1=Employee("sandy")
 emp1.info()
 emp2=Employee("Hari")
+emp2.raiseamt=4
 # Employee.company="Microsoft"
 emp2.company="Microsoft"
 
--- day67/main.py
+++ day67/main.py
@@ -1,1 +1,21 @@
 # Your Python code goes here
+# excercise 6 solution
+class Library:
+    def __init__(self) -> None:
+        self.noBooks=0
+        self.books=[]
+        
+    def addBooks(self,book):
+        self.books.append(book)
+        self.noBooks=len(self.books)
+        
+    def info(self):
+        print(f" The library has {self.noBooks} books and books are ")
+        for book in self.books:
+            print(book)
+        
+l1=Library()
+l1.addBooks("Harry Potters1")
+l1.addBooks("Harry Potters2")
+l1.addBooks("Harry Potters3")
+l1.info()
