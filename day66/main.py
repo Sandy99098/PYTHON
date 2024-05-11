7dc272109e78038e0873d313b427845ba7e9c968
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-11 14:58:00 +0545

    instance vs class variables

--- day66/main.py
+++ day66/main.py
@@ -1,1 +1,21 @@
 # Your Python code goes here
+
+# instance variables and class variable
+class Employee:
+    company="Appple"
+    number=0
+    def __init__(self ,name):
+        self.name=name
+        Employee.number+=1
+    def info(self):
+        print(f"{self.name} works at{self.company } and total employess are {Employee.number}") 
+        # print(f"{self.name} works at {Employee.company} ") 
+
+emp1=Employee("sandy")
+emp1.info()
+emp2=Employee("Hari")
+# Employee.company="Microsoft"
+emp2.company="Microsoft"
+
+emp2.info()
+
