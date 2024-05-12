9a09fdeda2052be1a20ba400bdd804744fdfa571
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-12 16:25:01 +0545

    class method in python

--- day68/main.py
+++ day68/main.py
@@ -1,1 +1,7 @@
 # Your Python code goes here
+# Excercise 7
+# write the program to clar the clutter inside a folder on your computer
+# you should use os model to rename all the png images form 1.png all 
+# the way tull n.png where n is the number of png  in that folder .
+# Do the same for other file formats
+
--- day69/main.py
+++ day69/main.py
@@ -1,1 +1,20 @@
 # Your Python code goes here
+#Class methods in python 
+class Employee:
+    company ="Apple"
+    name="Hari"
+    def show(self):
+        print(f"The name is {self.name} and company is {self.company}")
+        
+    @classmethod #by using this class variable is changed
+    def changeCompany(cls,newCompany):
+        cls.company=newCompany
+a=Employee()
+# print( a.name)
+a.show()
+a.changeCompany("Tesla")
+a.show()
+print(Employee.company)  # it will show the default value of company and donot change the value of the company . so we use classmethod decorator to change the class variable
+
+
+ 
