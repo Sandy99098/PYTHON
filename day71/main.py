f95c91cf35522afcc529d0bc902f5af0fe7861c3
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-13 15:03:15 +0545

    dir,__dict__,help method

--- day71/main.py
+++ day71/main.py
@@ -1,1 +1,15 @@
 # Your Python code goes here
+# dir, __dict__,help  methods
+x=[1,2,3,4]
+dir(x)
+print(dir(x)) #  return the list of the attributes 
+
+class hey():
+     def __init__(self,name,age ):
+         self.name=name 
+         self.age=age
+         self.verison=1
+p=hey("sdf",34)
+print(p.__dict__)
+print(f"dir in class is{dir(p)}")
+help(p)
