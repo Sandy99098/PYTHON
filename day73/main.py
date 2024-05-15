3631aed1ad9dfd41c6ddf0a20b093d419a49d644
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-15 14:07:32 +0545

    magic dunder methods

--- day73/main.py
+++ day73/main.py
@@ -1,1 +1,11 @@
 # Your Python code goes here
+# Dunder or magic methods in python ;
+
+
+from emp import Employee
+a=Employee("Sandy")
+print(a.name)
+# print(len(a))
+# print(a)
+print(repr(a))
+print(str(a))
