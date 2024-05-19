64fda02071a53985d41e17c3a45f3fb986fe089f
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-19 20:27:49 +0545

    ex 9 soln merge pdf

--- day82/main.py
+++ day82/main.py
@@ -1,1 +1,11 @@
 # Your Python code goes here
+#  merge the pdf
+from PyPDF2 import PdfWriter
+import os 
+merger =PdfWriter()
+files=[ file for file in os.listdir() if file.endswith('.pdf')]
+for pdf in ['file1.pdf','file2.pdf','file3.pdf']:
+    merger.append(pdf)
+    
+merger.wrtie('merged-pdf.pdf')
+merger.close()
