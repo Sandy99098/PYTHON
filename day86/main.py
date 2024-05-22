71d881e094a6cc55d6473f13b7b83e67e5616f74
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-22 21:01:25 +0545

    walrus operator

--- day86/main.py
+++ day86/main.py
@@ -1,1 +1,20 @@
-# Your Python code goes here
+#  warlus operator
+# a = False
+# # print(a=True) # type eroor occured sp use walrus operators
+
+# print(a:= True)
+
+# list =[1,34,5,6,5,7]
+# while (n:= len(list) )>0:
+#     print(list.pop())
+    
+    
+    #  walrus operator for taling customer's order 
+order=list()
+while(o:= input("Enter your order ") )!="quit":
+    (order.append(o))
+
+while(l:=len(order))>0: 
+    print("Your orders list is")   
+    print(order.pop())
+
