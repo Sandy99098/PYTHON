d4727b703bea2516973d1218ea5584d1e1926867
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-23 15:13:45 +0545

    shutil method

--- day87/main.py
+++ day87/main.py
@@ -1,1 +1,19 @@
-# Your Python code goes here
+#Shutil method in Python  -  high ;eve; file operations
+import shutil
+
+shutil.copy("main.py","main2.py")
+
+shutil.copy2("main.py","main2.py")  #details wil go at the meta data
+
+shutil.copytree("main.py","main2.py") # copy the folder 
+
+shutil.move(",tutorial/file.file","file.file")
+
+
+
+shutil.rmtree("main.py") # use for file directory 
+
+
+import os
+os.remove("helo.txt")
+
