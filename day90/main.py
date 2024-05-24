a826c5e07b4e26f5295f623cd14c9ce64de83980
Sandy99098 <waglesandeeep8@gmail.com>
2024-05-24 22:13:34 +0545

    news api

--- day90/main.py
+++ day90/main.py
@@ -1,1 +1,50 @@
-# Your Python code goes here
+# # excercise 10
+# use the request module 
+# use the news api  to fetch the daily news related to different topicd 
+# got to https://newsapi.org/
+# and explore the variouse options to build the various options to buold your application
+
+
+# import requests 
+# import json
+
+# API_KEY = "2537c7d5154e4f02846929e5b064e6b4"
+# url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+
+# r = requests.get(url)
+# data = json.loads(r.content)
+# print(data)
+# for i in range(len(data)):
+#     i=1
+#     print(i,data['articles'][i]['title'])
+#     print(i,data['articles'][i]['description'])
+#     i=i+1
+
+# import requests 
+
+# API_KEY = "2537c7d5154e4f02846929e5b064e6b4"
+# url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+
+# response = requests.get(url)
+# data = response.json()
+
+# articles = data['articles']
+# for i, article in enumerate(articles, start=1):
+#     print(f"{i}. Title: {article['title']}")
+#     print("   Description:", article['description'])
+#     print()
+
+
+import requests 
+
+API_KEY = "2537c7d5154e4f02846929e5b064e6b4"
+url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+
+response = requests.get(url)
+data = response.json()
+
+articles = data['articles']
+for article in articles:
+    print("Title:", article['title'])
+    print("Description:", article['description'])
+    print()
